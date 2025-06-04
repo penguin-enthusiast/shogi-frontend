a608f0be2e0515068afc7668f6b2e3a683264570
penguin-enthusiast <212586688+penguin-enthusiast@users.noreply.github.com>
2025-06-04 15:58:09 -0400

    add turn based moving for standard shogi

--- src/ReadyPanel.tsx
+++ src/ReadyPanel.tsx
@@ -2,32 +2,43 @@ import {useContext, useEffect, useState} from "react";
 import type {Game} from "./types/types.ts";
 import {ClientContext} from "./Contexts.ts";
 import './assets/css/ready-panel.css';
+import type {Color} from "shogiground/types";
 
-function ReadyPanel ({game, playerId}: {game: Game | null, playerId: string}) {
+function ReadyPanel ({game, playerId, turn}: {game: Game | null, playerId: string, turn: Color}) {
     const stompClientRef = useContext(ClientContext);
 
     const [readyMessage, setReadyMessage] = useState("Click when ready");
     const [ready, setReady] = useState(false);
     const [buttonClass, setButtonClass] = useState("button-not-ready");
     const [opponentReady, setOpponentReady] = useState(false);
+    const [player, setPlayer] = useState<Color>('sente');
+    const [moveMessage, setMoveMessage] = useState('');
 
     useEffect(() => {
         resetPanel();
     }, []);
 
     useEffect(() => {
-        if (game == null) {
+        if (game == null || game.status == "FINISHED") {
+            resetPanel();
             return;
         }
-        if (game.status == "FINISHED") {
-            resetPanel();
-        }
         if (playerId == game.player1) {
+            setPlayer('sente');
             setOpponentReady(game.player2Ready);
         } else if (playerId == game.player2) {
+            setPlayer('gote');
             setOpponentReady(game.player1Ready);
         }
     }, [game, playerId])
+
+    useEffect(() => {
+        if (!game || game.status != "IN_PROGRESS") {
+            setMoveMessage('');
+            return;
+        }
+        setMoveMessage(player == turn ? "Your move." : "Waiting for opponent.");
+    }, [game, player, turn]);
 
     const resetPanel = () => {
         setReady(false);
@@ -74,6 +85,7 @@ function ReadyPanel ({game, playerId}: {game: Game | null, playerId: string}) {
             <p className={opponentReady ? 'text-ready' : ''}>
                 {opponentReady? "Opponent is ready." : "Waiting for opponent..."}
             </p>
+            <p>{moveMessage}</p>
         </div>
     );
 }
